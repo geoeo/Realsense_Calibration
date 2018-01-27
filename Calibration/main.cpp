b34be4effbff3c608e10709772ac7b5abbeb1ebd
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2018-01-27 16:28:19 +0100

    can now calculate extrinsics given an image and camera intrinsics

--- Calibration/main.cpp
+++ Calibration/main.cpp
@@ -16,28 +16,61 @@
 #include "calibration.hpp"
 #include "definitions.hpp"
 
+const int FAIL = 1;
+
+int loadAllImagesFromDir(const string dirPath, vector<string>& imagePaths, vector<Mat>& images){
+    
+    bool success = loadFilesInDirInto(imagePaths,dirPath);
+    if(success)
+        loadImages(imagePaths,images);
+    else
+        return FAIL;
+    
+    return 0;
+    
+}
+
+int loadSingleImageFromDir(string imagePath,vector<string>& imagePaths, vector<Mat>& images){
+    
+    imagePaths.push_back(imagePath);
+    
+    if(!loadImages(imagePaths,images))
+       return FAIL;
+
+
+    return 0;
+    
+}
+
 
 int main()
 {
-    const int FAIL = 1;
+
     vector<Mat> images;
     Mat intrinsics = Mat::eye(3, 3, CV_64F);
     Mat distortion = Mat::zeros(5, 1, CV_64F);;
     
-    string intrinsicsFolder = "Calibration_Output/defaults/";
+    //string intrinsicsFolder = "Calibration_Output/defaults/";
+    string intrinsicsFolder = "Calibration_Output/Seq_1/";
+    
     vector<string> imagePaths;
     bool success;
     string dirPath;
     string root = returnRoot();
+    string imagePath;
 
+    imagePath.append(root).append("Image_Seq_1/").append("image_21.png");
     dirPath.append(root).append("Image_Seq_1/");
-    // change this to calcuate rms for the default intrinsics on a given image set
+    
+    // change this to calibrate the camera on a given image set
     bool useSuppliedParamters = true;
 
-    success = loadFilesInDirInto(imagePaths,dirPath);
-    if(success)
-        loadImages(imagePaths,images);
-    else
+//    //Used for calibration
+//    if(loadAllImagesFromDir(dirPath,imagePaths,images) == FAIL)
+//        return FAIL;
+    
+    // Used to estimate camera extrinsics for single image
+    if(loadSingleImageFromDir(imagePath,imagePaths, images) == FAIL)
         return FAIL;
     
     loadIntrinsicsInto(intrinsics, distortion, intrinsicsFolder);
